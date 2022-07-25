ef0c353e1f31a36643f057f0669e037ccf105920
Sunkyung Cho <sunkyungch@gmail.com>
2022-07-25 16:34:45 +0900

    first commit

---
+++ src/main/java/me/example/userauthenticatetest/UserAuthenticateTestApplication.java
@@ -0,0 +1,13 @@
+package me.example.userauthenticatetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticateTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAuthenticateTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/example/userauthenticatetest/UserAuthenticateTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.example.userauthenticatetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthenticateTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
